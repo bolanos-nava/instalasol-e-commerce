ce895fa087aac02bd1334a24ddd9e91462bddf8f
bolanos.nava <bolanos.nava@gmail.com>
2024-01-29 18:40:57 -0600

    Adds ItemListContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
 import CartWidget from './components/CartWidget';
 import NavBar from './components/NavBar';
+import { ItemListContainer } from './components/Items/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
-    <NavBar>
-      <CartWidget />
-    </NavBar>
+    <>
+      <NavBar>
+        <CartWidget />
+      </NavBar>
+      <ItemListContainer greeting="Bienvenido a tu tienda en línea - consigue todo para tu proyecto de instalación de paneles solares aquí" />
+    </>
   );
 }
 
