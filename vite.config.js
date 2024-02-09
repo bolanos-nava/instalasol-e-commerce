8e7ee1e68d2f1a105f2ad156935f3895884ee72a
bolanos.nava <bolanos.nava@gmail.com>
2024-02-09 01:20:43 -0600

    npm(install): Installs Sass and configures Vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import sass from 'sass';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -26,6 +27,13 @@ export default defineConfig(({ mode }) => {
         },
       }),
     ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          implementation: sass,
+        },
+      },
+    },
     server: { port: 3000 },
     base: env.BASE_URL,
   };
