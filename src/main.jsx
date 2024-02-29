9ab267e7262411c235f0b927ecdffacfbbe13460
bolanos.nava <bolanos.nava@gmail.com>
2024-02-29 16:02:15 -0600

    config: Moves Firebase API key to env and changes Firebase App data

--- src/main.jsx
+++ src/main.jsx
@@ -7,21 +7,20 @@ import ReactDOM from 'react-dom/client';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import App from './App';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDdCYLDEbpIegsQ42TnyGiCMryiX_ThdLM',
-  authDomain: 'coderhouse-prueba-bbde0.firebaseapp.com',
-  projectId: 'coderhouse-prueba-bbde0',
-  storageBucket: 'coderhouse-prueba-bbde0.appspot.com',
-  messagingSenderId: '370735572271',
-  appId: '1:370735572271:web:933c10ab099dd377b62dda',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: 'instalasol-e-store.firebaseapp.com',
+  projectId: 'instalasol-e-store',
+  storageBucket: 'instalasol-e-store.appspot.com',
+  messagingSenderId: '332823164213',
+  appId: '1:332823164213:web:271b3159af3af73b6b487c',
+  measurementId: 'G-EKN85QCPLD',
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 // ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 ReactDOM.createRoot(document.getElementById('root')).render(
