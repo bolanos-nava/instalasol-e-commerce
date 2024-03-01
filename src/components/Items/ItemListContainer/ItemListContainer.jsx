cad82ada038d9f2406548684436994d130f728d2
bolanos.nava <bolanos.nava@gmail.com>
2024-03-01 02:33:51 -0600

    feat(context): Uses context in ItemListContainer to retrieve global categories

--- src/components/Items/ItemListContainer/ItemListContainer.jsx
+++ src/components/Items/ItemListContainer/ItemListContainer.jsx
@@ -1,84 +1,70 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
+import { doc, getFirestore, where } from 'firebase/firestore';
 
 import { BootstrapProgress } from '../../styled-components';
-import { fetchCollection, fetchMock } from '../../../utils/utils';
+import { fetchCollection } from '../../../utils/utils';
 import { ItemList } from '../ItemList';
 import { ErrorHandler } from '../../Errors';
-import { where } from 'firebase/firestore';
+import { FetchContext } from '../../../contexts/FetchContext';
 
 export function ItemListContainer() {
   const { categoryCode } = useParams();
   const location = useLocation();
+  const { categories, isFetching: areCategoriesFetching } =
+    useContext(FetchContext);
   const [errors, setErrors] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [categorySelected, setCategorySelected] = useState({});
   const [isLoading, setIsLoading] = useState(true);
 
-  function onFetchFulfilled(responses) {
-    const currentCategory = responses[0].find(
-      (category) => category.code === categoryCode,
-    );
-    if (currentCategory) {
-      setCategorySelected(currentCategory ?? {});
-      setFilteredProducts(
-        responses[1].filter(
-          (product) => product.product_category_id === currentCategory.id,
-        ),
-      );
-    } else {
-      setErrors([{ message: 'Categoría no encontrada', status: 404 }]);
-    }
-  }
   function onFetchRejected(response) {
     setErrors(response);
   }
 
   function onProductsFulfilled(response) {
-    const products = response.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
+    const products = response.docs.map((docu) => ({
+      id: docu.id,
+      ...docu.data(),
     }));
     setFilteredProducts(products);
   }
 
   useEffect(() => {
-    const getProducts = () =>
-      fetchMock(`${process.env.BASE_URL}/mockData/products.json`);
-    const getCategories = () =>
-      fetchMock(`${process.env.BASE_URL}/mockData/categories.json`);
+    const db = getFirestore();
+    if (!areCategoriesFetching) {
+      let categoryFilters = [];
+      if (categoryCode) {
+        const currentCategory = categories.find(
+          (category) => category.code === categoryCode,
+        );
+        // this operator will leave the categorySelected as an empty object in case the category
+        // param doesn't actually exist
+        setCategorySelected(currentCategory ?? {});
+        categoryFilters = [
+          where(
+            'product_category_id',
+            '==',
+            doc(
+              db,
+              'categories',
+              currentCategory ? currentCategory.id : 'invalid_category',
+            ),
+          ),
+        ];
+      }
 
-    const categoryFilters = categoryCode
-      ? [where('code', '==', categoryCode)]
-      : [];
-    const fetchProducts = () =>
       fetchCollection('products', {
         errorMessage: 'Productos no encontrados para esta categoría',
         filters: categoryFilters,
-      });
-
-    // if (categoryCode) {
-    //   // const promises = [getCategories(), getProducts()];
-    //   // Promise.all(promises)
-    //   //   .then(onFetchFulfilled, onFetchRejected)
-    //   //   .finally(() => {
-    //   //     setIsLoading(false);
-    //   //   });
-    // } else {
-    //   fetchProducts()
-    //     .then(onProductsFulfilled, onFetchRejected)
-    //     .finally(() => setIsLoading(false));
-    // }
-
-    fetchCollection('products', {
-      errorMessage: 'Productos no encontrados para esta categoría',
-      filters: categoryFilters,
-    })
-      .then(onProductsFulfilled, onFetchRejected)
-      .finally(() => setIsLoading(false));
-  }, [categoryCode, location]);
+      })
+        .then(onProductsFulfilled, onFetchRejected)
+        .finally(() => setIsLoading(false));
+    }
+  }, [categoryCode, location, areCategoriesFetching, categories]);
 
   useEffect(() => {
+    setErrors([]);
     setIsLoading(true);
   }, [location]);
 
